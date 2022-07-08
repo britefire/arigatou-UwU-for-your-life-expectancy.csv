621224092627847e26e6b190de33fea7f5986b85
britefire <twwigevergreen@gmail.com>
2022-07-07 21:35:35 -0500

    Update life-expectancy.py
    owO

--- life-expectancy.py
+++ life-expectancy.py
@@ -1,36 +1,87 @@
-with open("life-expectancy.csv", "r") as file:
-    file.readline()
-    years = []
-    countries = []
-    expectancies = []
+[opening music]
 
-    for line in file:
-        line = line.strip()
-        line = line.split(",")
+[wind]
 
-        country = line[0]
-        year = line[2]
-        expectancy = line[3]
+[clop clop clop]
 
-        countries.append(country)
-        years.append(year)
-        expectancies.append(expectancy)
+ARTHUR: Whoa there!
 
+[clop clop clop]
 
-year_of_interest = int(input("Please enter the year of interest: "))
-print()
-print(f"The overall max life expectancy is: {max(expectancies)} from {countries[expectancies.index(max(expectancies))]} in the year {years[expectancies.index(max(expectancies))]}.")
-print(f"The overall max life expectancy is: {min(expectancies)} from {countries[expectancies.index(min(expectancies))]} in the year {years[expectancies.index(min(expectancies))]}.")
-print()
-print(f"From the year {year_of_interest}:")
-year_of_interest_list = []
-total = 0
-for element in range(len(years)):
-    if year_of_interest == float(years[element]):
-        year_of_interest_list.append(expectancies[element])
+SOLDIER #1: Halt! Who goes there?
 
-for e in year_of_interest_list:
-    total += float(e)
-print(f"The average life expectancy across all countries was {round(total/len(year_of_interest_list),2)}")
-print(f"The maximum life expectancy was in {countries[expectancies.index(max(year_of_interest_list))]} in the year {max(year_of_interest_list)}")
-print(f"The maximum life expectancy was in {countries[expectancies.index(min(year_of_interest_list))]} in the year {min(year_of_interest_list)}")
+ARTHUR: It is I, Arthur, son of Uther Pendragon, from the castle of Camelot. King of the Britons, defeator of the Saxons, sovereign of all England!
+
+SOLDIER #1: Pull the other one!
+
+ARTHUR: I am. And this my trusty servant Patsy. We have ridden the length and breadth of the land in search of knights who will join me in my court of Camelot. I must speak with your lord and master.
+
+SOLDIER #1: What, ridden on a horse?
+
+ARTHUR: Yes!
+
+SOLDIER #1: You're using coconuts!
+
+ARTHUR: What?
+
+SOLDIER #1: You've got two empty halves of coconut and you're bangin' 'em together.
+
+ARTHUR: So? We have ridden since the snows of winter covered this land, through the kingdom of Mercea, through--
+
+SOLDIER #1: Where'd you get the coconut?
+
+ARTHUR: We found them.
+
+SOLDIER #1: Found them? In Mercea? The coconut's tropical!
+
+ARTHUR: What do you mean?
+
+SOLDIER #1: Well, this is a temperate zone.
+
+ARTHUR: The swallow may fly south with the sun or the house martin or the plumber may seek warmer climes in winter yet these are not strangers to our land.
+
+SOLDIER #1: Are you suggesting coconuts migrate?
+
+ARTHUR: Not at all, they could be carried.
+
+SOLDIER #1: What -- a swallow carrying a coconut?
+
+ARTHUR: It could grip it by the husk!
+
+SOLDIER #1: It's not a question of where he grips it! It's a simple question of weight ratios! A five ounce bird could not carry a 1 pound coconut.
+
+ARTHUR: Well, it doesn't matter. Will you go and tell your master that Arthur from the Court of Camelot is here.
+
+SOLDIER #1: Listen, in order to maintain air-speed velocity, a swallow needs to beat its wings 43 times every second, right?
+
+ARTHUR: Please!
+
+SOLDIER #1: Am I right?
+
+ARTHUR: I'm not interested!
+
+SOLDIER #2: It could be carried by an African swallow!
+
+SOLDIER #1: Oh, yeah, an African swallow maybe, but not a European swallow. That's my point.
+
+SOLDIER #2: Oh, yeah, I agree with that.
+
+ARTHUR: Will you ask your master if he wants to join my court at Camelot?!
+
+SOLDIER #1: But then of course, uh, African swallows are non-migratory.
+
+SOLDIER #2: Oh, yeah.
+
+SOLDIER #1: So, they couldn't bring a coconut back anyway.
+
+[clop clop clop]
+
+SOLDIER #2: Wait a minute! Supposing two swallows carried it together?
+
+SOLDIER #1: No, they'd have to have it on a line.
+
+SOLDIER #2: Well, simple! They'd just use a strand of creeper!
+
+SOLDIER #1: What, held under the dorsal guiding feathers?
+
+SOLDIER #2: Well, why not? 
